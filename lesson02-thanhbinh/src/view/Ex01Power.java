b1e36c5c1d31566ac3e001a97aca4e6f5ff9abe8
thanhbinhh105 <thanhbinhh105@gmail.com>
2022-05-12 00:07:40 +0700

    lesson02-thanhbinh 11.05.2022

--- lesson02-thanhbinh/src/view/Ex01Power.java
+++ lesson02-thanhbinh/src/view/Ex01Power.java
@@ -2,28 +2,17 @@ package view;
 
 public class Ex01Power {
     public static void main(String[] args) {
-        int[] elements = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 32};
-        for (int number : elements) {
-            boolean isPower = isPowerofTwo(number);
-            System.out.println("==>" + isPower);
+        int a = 35;
+        if (isPower(a)) {
+            System.out.println("la luy thua 2");
+        } else {
+            System.out.println("khong la luy thua2");
         }
-
-        System.out.println("===========================================");
-
-        System.out.println("8-2" + isPower2(8, 2));
-        System.out.println("8-3" + isPower2(8, 3));
-        System.out.println("7-2" + isPower2(7, 2));
-        System.out.println("5-2" + isPower2(5, 2));
     }
 
-    private static boolean isPowerofTwo(int number) {
-        double log = Math.log(number) / Math.log(2);
-        System.out.println(number + "==>" + log);
-        return Math.ceil(log) == Math.round(log);
-    }
-
-    private static boolean isPower2(int a, int b) {
-        double log = a > b ? Math.log(a) / Math.log(b) : Math.log(b) / Math.log(a);
+    private static boolean isPower(int n) {
+        double log = Math.log(n) / Math.log(2);
         return Math.ceil(log) == Math.round(log);
     }
 }
+
