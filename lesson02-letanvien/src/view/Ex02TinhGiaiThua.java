de9b4e95185583762e2b7713ceba176d426af9d5
Vienlt <tanvien93lt@gmail.com>
2022-05-12 21:06:21 +0700

    lesson02

--- lesson02-letanvien/src/view/Ex02TinhGiaiThua.java
+++ lesson02-letanvien/src/view/Ex02TinhGiaiThua.java
@@ -3,20 +3,12 @@ package view;
 public class Ex02TinhGiaiThua {
 
 	public static void main(String[] args) {
-		int a = 4;
-		int b = 7;
-		int c = 12;
-		int d = 18;
-		long aGiaiThua = factorial(a);
-		long bGiaiThua = factorial(b);
-		long cGiaiThua = factorial(c);
-		long dGiaiThua = factorial(d);
-		System.out.println("Factorial of a is : " + aGiaiThua);
-		System.out.println("Factorial of b is : " + bGiaiThua);
-		System.out.println("Factorial of c is : " + cGiaiThua);
-		System.out.println("Factorial of d is : " + dGiaiThua);
-		long FactorialTotal = aGiaiThua + bGiaiThua + cGiaiThua + dGiaiThua;
-		System.out.println("Factorial Total : " + FactorialTotal);
+	
+		long result = factorial(4) + factorial(7) + factorial(12) + factorial(18);
+				
+		
+	
+		System.out.println("Factorial Total : " + result);
 	}
 
 	public static long factorial(int n) {
