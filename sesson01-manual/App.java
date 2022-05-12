763bcb3f960e5d0d358c7622fc67d145233c224d
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-05-12 17:00:50 +0700

    remove unexpected projects 12.05.2022

--- sesson01-manual/App.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class App {
-   public static void main(String[] args) {
-         System.out.println("Welcome to Java class");   
-	 System.out.println("Today: " + LocalDate.now());
-   }
-}
