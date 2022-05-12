7d67e79182db213edb508d0347283beb9a1dcc3d
dquoc <ducquocvyy@gmail.com>
2022-05-13 00:01:26 +0700

    lesson02-quocvyy 13.05.2022

---
+++ lesson02-quocvyy/src/view/Ex05.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		int n ;
+		Scanner ip = new Scanner(System.in);
+		System.out.println("nhập số nguyên n: ");
+		n = ip.nextInt();
+		if(n < 0) {
+			System.out.println("nhập lại n: ");
+		}
+		
+
+	}
+
+}
+
