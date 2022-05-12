2dd5c6fad332e7f8803f1d574921cd3014cfd977
dquoc <ducquocvyy@gmail.com>
2022-05-13 00:01:26 +0700

    lesson02-quocvyy 13.05.2022

---
+++ lesson02-quocvyy/src/view/Ex02.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex02 {
+
+	  
+    public static long tinhGiaiThua(int n) {
+        long gt = 1;
+		Scanner ip = new Scanner(System.in);
+		System.out.println("nhap n: ");
+		n = ip.nextInt();
+        for (int i = 1; i <= n; i++) {
+            gt *= i;
+        }
+        return gt;
+    }
+
+    public static void main(String[] args) {
+        long S = 0;
+        for (int i = 1; i <= 44
+        		; i++) {
+            S += tinhGiaiThua(i);
+        }
+        System.out.println("Tong cac giai thua la: " + S);
+    }
+
+}
