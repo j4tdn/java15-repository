35bcd1296af446795ab69f14645def6dbebcb1b1
dungpro <dungpro@gamil.com>
2022-05-13 22:01:40 +0700

    lesson03-oop 13.05.2022

---
+++ lesson03-oop/src/view/Ex01RectApp.java
@@ -0,0 +1,19 @@
+package view;
+
+import bean.Rectangle;
+
+public class Ex01RectApp {
+public static void main(String[] args) {
+Rectangle r1 =new Rectangle();
+r1.setHeight(12);
+r1.setWidth(10);
+Rectangle r2 =new Rectangle();
+r1.setHeight(10);
+r1.setWidth(15);
+	Rectangle r3 = new Rectangle(12, 7);
+
+	
+	
+
+}
+}
