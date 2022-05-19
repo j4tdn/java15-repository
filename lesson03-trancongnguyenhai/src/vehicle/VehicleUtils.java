83c2c6423167cd1213dc5a36e98e0e180112f07d
HaiTran <nguyenhai200902@gmail.com>
2022-05-19 17:58:41 +0700

    lesson03-trancongnguyenhai 19.05.2022

--- lesson03-trancongnguyenhai/src/vehicle/VehicleUtils.java
+++ lesson03-trancongnguyenhai/src/vehicle/VehicleUtils.java
@@ -1,6 +1,12 @@
 package vehicle;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class VehicleUtils {
+	public static Scanner ip = new Scanner(System.in);
+
 	public static double regisTax(Vehicle v) {
 		if (v.getCylin() < 100) {
 			return v.getPrice() - v.getPrice() * 0.01;
@@ -9,9 +15,27 @@ public class VehicleUtils {
 		}
 		return v.getPrice() - v.getPrice() * 0.05;
 	}
-	
-	public static String importRecord(Vehicle v) {
-			String col = String.format("%-25s%-25s%-25s%-25s%-25s", v.getNameDriver(), v.getType(),v.getCylin(), v.getPrice(), regisTax(v));
-			return col;
+
+	public static void showRecord(Vehicle[] record) {
+		for (int i = 0; i < record.length; i++) {
+			String col = String.format("%-25s%-25s%-25s%-25s%-25s", record[i].getNameDriver(), record[i].getType(),
+					record[i].getCylin(), record[i].getPrice(), regisTax(record[i]));
+			System.out.println(col);
+		}
+	}
+
+	public static void writeRecord(Vehicle v) {
+		System.out.print("Nhập tên tài xế: ");
+		String nameDriver = ip.nextLine();
+		v.setNameDriver(nameDriver);
+		System.out.print("Nhập loại xe: ");
+		String type = ip.nextLine();
+		v.setType(type);
+		System.out.print("Nhập dung tích: ");
+		int cylin = ip.nextInt();
+		v.setCylin(cylin);
+		System.out.print("Nhập giá: ");
+		double price = ip.nextDouble();
+		v.setPrice(price);
 	}
 }
