8a1427504f3a8ff89df3f04ef36f7f99f029405f
danghuynhtouyen <danghuynhtouyen@gmail.com>
2022-06-05 21:27:54 +0700

    lesson05-hoten 05.06.2022

---
+++ lesson05-hoten/src/exercise01/exercise01.java
@@ -0,0 +1,13 @@
+package exercise01;
+
+public class exercise01 {
+	public static void main(String[] args) {
+		System.out.println("Bai 1: Viet ham kiem tra mot so có phai la luy thua cua mot so khac khong "
+				+"VD: (8,2)(true),(2,8)(true),(6,2)(false),(20,4)(false),(64,4)(true)");
+		System.out.println("Bai lam: ");
+		int a = 8;
+		System.out.println("a: " + a);
+		int b = 2;
+		System.out.println("b: " + b);
+		}
+}
