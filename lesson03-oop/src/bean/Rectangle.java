108a0e4a08ddd0b8781f73a85f638b73ba8698ae
dungpro <dungpro@gamil.com>
2022-06-05 19:32:21 +0700

    lesson03-oop 5.6.2022

--- lesson03-oop/src/bean/Rectangle.java
+++ lesson03-oop/src/bean/Rectangle.java
@@ -1,35 +1,37 @@
 package bean;
 
 public class Rectangle {
-private int width;
-private int height;
-public Rectangle() {
-	
-}
+	private int width;
+	private int height;
 
-public Rectangle(int width, int height) {
-	this.width =width;
-	this.height = height;
-	
-}
+	public Rectangle() {
 
-public int getWidth() {
-	return width;
-}
+	}
 
-public void setWidth(int width) {
-	this.width = width;
-}
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
 
-public int getHeight() {
-	return height;
-}
+	}
 
-public void setHeight(int height) {
-	this.height = height;
-}
-@Override
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	@Override
 	public String toString() {
-	return width +" " + height;
+		return width + " " + height;
 	}
 }
---
+++ lesson03-oop/src/view/Ex04StaticMethod.java
@@ -0,0 +1,14 @@
+package view;
+
+public class Ex04StaticMethod {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+
+//		System.out.println("r1: " +);
+	}
+
+	private static int sum(int a, int b) {
+		return a + b;
+	}
+}
