244bdd0374b27c6ede331eb7fa1b1a6f6e5a4dc2
Vienlt <tanvien93lt@gmail.com>
2022-07-08 19:20:20 +0700

    lesson07-libraries

---
+++ lesson07-libraries/src/datetime/Ex01CodeDemo.java
@@ -0,0 +1,29 @@
+package datetime;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Date(java.util.lang) Calender SimpleDateFormat
+ */
+public class Ex01CodeDemo {
+	public static void main(String[] args) {
+		// get current date time
+		Date date = new Date();
+		System.out.println("date: " + date);
+		Date dateWithMiliseconds = new Date(24 * 60 * 60 * 1000);
+		System.out.println("dateWithMiliseconds: " + dateWithMiliseconds);
+
+		System.out.println("===========================");
+		Calendar c = Calendar.getInstance();
+		System.out.println("now: " + c);
+
+		Calendar cWithTime = Calendar.getInstance();
+		cWithTime.set(2025, Calendar.AUGUST, 19);
+		cWithTime.set(Calendar.MONTH, Calendar.JANUARY);
+		cWithTime.set(Calendar.YEAR, 2022);
+		cWithTime.add(Calendar.DAY_OF_MONTH, 6);
+
+		System.out.println("now :" + cWithTime);
+	}
+}
