ffab848fda1dc74ef31c51979819a42c4fa3eef7
Ánh Tuyết <lethianhtuyet0210@gmail.com>
2022-08-03 14:35:15 +0700

    lesson08-anhtuyet 03.08.2022

---
+++ lesson08-anhtuyet/src/Ex03.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Ex03 {
+	private static final char[] SCOURCE_CHARACTER = {'À', 'Á', 'Â', 'Ã', 'È', 'É', 'Ê', 'Ì', 'Í', 'Ò', 'Ó', 'Ô', 'Õ',
+			'Ù', 'Ú', 'Ý', 'à', 'á', 'â', 'ã', 'è', 'é', 'ê', 'ì', 'í', 'ò', 'ó', 'ô', 'õ', 'ù', 'ú', 'ý', 'Ă', 'ă',
+			'Đ', 'đ', 'Ĩ', 'ĩ', 'Ũ', 'ũ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ạ', 'ạ', 'Ả', 'ả', 'Ấ', 'ấ', 'Ầ', 'ầ', 'Ẩ', 'ẩ', 'Ẫ',
+			'ẫ', 'Ậ', 'ậ', 'Ắ', 'ắ', 'Ằ', 'ằ', 'Ẳ', 'ẳ', 'Ẵ', 'ẵ', 'Ặ', 'ặ', 'Ẹ', 'ẹ', 'Ẻ', 'ẻ', 'Ẽ', 'ẽ', 'Ế', 'ế',
+			'Ề', 'ề', 'Ể', 'ể', 'Ễ', 'ễ', 'Ệ', 'ệ', 'Ỉ', 'ỉ', 'Ị', 'ị', 'Ọ', 'ọ', 'Ỏ', 'ỏ', 'Ố', 'ố', 'Ồ', 'ồ', 'Ổ',
+			'ổ', 'Ỗ', 'ỗ', 'Ộ', 'ộ', 'Ớ', 'ớ', 'Ờ', 'ờ', 'Ở', 'ở', 'Ỡ', 'ỡ', 'Ợ', 'ợ', 'Ụ', 'ụ', 'Ủ', 'ủ', 'Ứ', 'ứ',
+			'Ừ', 'ừ', 'Ử', 'ử', 'Ữ', 'ữ', 'Ự', 'ự',};
+	private static final char[] DESTINATION_CHARACTERS = {'A', 'A', 'A', 'A', 'E', 'E', 'E', 'I', 'I', 'O', 'O', 'O',
+			'O', 'U', 'U', 'Y', 'a', 'a', 'a', 'a', 'e', 'e', 'e', 'i', 'i', 'o', 'o', 'o', 'o', 'u', 'u', 'y', 'A',
+			'a', 'D', 'd', 'I', 'i', 'U', 'u', 'O', 'o', 'U', 'u', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a',
+			'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'A', 'a', 'E', 'e', 'E', 'e', 'E', 'e', 'E',
+			'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'I', 'i', 'I', 'i', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o',
+			'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'O', 'o', 'U', 'u', 'U', 'u', 'U',
+			'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u',};
+	public static void main(String[] args) {
+		String s = "Em có yêu anh không anh để anh biết còn chờ\r\n" 
+					+ "Em xin lỗi em đã có thái độ không đúng với anh";
+		System.out.println(covertToNormalLetters(s));
+	}
+	private static char covertToNormalLetters(char s) {
+		int index=Arrays.binarySearch(SCOURCE_CHARACTER, s);
+		if(index >=0) {
+			s = DESTINATION_CHARACTERS[index];
+		}
+		return s;
+	}
+	private static String covertToNormalLetters(String s) {
+		StringBuilder sb = new StringBuilder(s);
+		for(int i = 0; i < s.length(); i++) {
+			sb.setCharAt(i,covertToNormalLetters(sb.charAt(i)));
+		}
+		return sb.toString();
+	}
+}
---
+++ lesson08-anhtuyet/src/Ex07.java
@@ -0,0 +1,16 @@
+
+public class Ex07 {
+	public static void main(String[] args) {
+		String s ="aaabaaabbaaaaa";
+	}
+	private static void subSequence(String s) {
+		int count = 0;
+		int max = 0; 
+		int min = s.length();
+		int startMax = 0;
+		int endMax = 0; 
+		int startMin = 0;
+		int endMin = 0;
+	
+			}
+}
