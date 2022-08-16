7f44af94064d96b0cbc41dad259ed373064770c2
hieupham <chinhhieu0906@gmail.com>
2022-08-16 15:08:12 +0700

    lesson10-arrays 16.08.2022

--- lesson10-array/src/view/Ex07_ArraysSortWithItemDetailV2.java
+++ lesson10-array/src/view/Ex07_ArraysSortWithItemDetailV2.java
@@ -1,10 +1,19 @@
 package view;
 
+import java.util.Arrays;
+import static java.util.Comparator.*;
+
 import bean.ItemDetail;
 import model.DataModel;
 
 public class Ex07_ArraysSortWithItemDetailV2 {
 	public static void main(String[] args) {
 		ItemDetail[] items = DataModel.getItemDetail();
+		Arrays.sort(items, nullsFirst(
+				
+				comparing(ItemDetail::getStoreId, reverseOrder())
+				.thenComparing(ItemDetail::getItemId, reverseOrder()))
+				);
+		System.out.println(Arrays.toString(items));
 	}
 }
