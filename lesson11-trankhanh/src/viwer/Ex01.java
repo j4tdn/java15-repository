81229d0edfc073e68f8ec33a8fddb93344d1d86a
trankhanh <khanh1234bln@gmail.com>
2022-09-04 21:51:28 +0700

    lesson11-trankhanh 04.09.2022

---
+++ lesson11-trankhanh/src/viwer/Ex01.java
@@ -0,0 +1,51 @@
+package viwer;
+
+public class Ex01 {
+
+	
+
+		public static void main(String[] args) {
+			for (int j = 1; j <= 5; j++) {
+
+				for (int k = 1; k <= j; k++) { 
+
+					System.out.print("*");
+
+				}
+
+				for (int k = 5; k >= j; k--) { 
+					System.out.print("  ");
+				}
+
+				for (int k = 1; k <= j; k++) {
+
+					System.out.print("*");
+
+				}
+				System.out.println();
+			}
+
+			for (int j = 1; j <= 4; j++) { 
+
+				for (int k = 4; k >= j; k--) { 
+
+					System.out.print("*");
+
+				}
+
+				for (int k = 0; k <= j; k++) { 
+					System.out.print("  ");
+				}
+
+				for (int k = 4; k >= j; k--) {
+
+					System.out.print("*");
+
+				}
+
+				System.out.println();
+			}
+		}
+
+	}
+
