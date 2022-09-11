4e116d46e601081afb7d69eb726783d7f2791fc4
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-09-11 19:24:18 +0700

    lesson12-stream init 11.09.2022

---
+++ lesson12-stream/src/model/DataModel.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.Arrays;
+import java.util.List;
+
+import bean.Apple;
+import bean.Dish;
+import bean.Dish.Kind;
+import bean.Trader;
+import bean.Transaction;
+
+public class DataModel {
+	private DataModel() {
+		
+	}
+	
+	public static List<Apple> getApples() {
+		return Arrays.asList(
+				new Apple(1, "green", 200, "Vietnam"),	
+				new Apple(16, "green", 300, "Vietnam"),	
+				new Apple(22, "red", 300, "Lao"),	
+				new Apple(4, "yellow", 400, "Thailand"),	
+				new Apple(17, "red", 600, "Vietnam"),	
+				new Apple(6, "green", 100, "Thailand")	
+		);
+	}
+	
+	public static List<Dish> getDishes() {
+		return Arrays.asList(
+				new Dish("1", "D1", 308, Kind.FISH, false),
+				new Dish("2", "D2", 620, Kind.OTHER, true),
+				new Dish("3", "D3", 329, Kind.OTHER, true),
+				new Dish("4", "D4", 308, Kind.MEAT, false),
+				new Dish("5", "D5", 480, Kind.MEAT, false),
+				new Dish("6", "D6", 480, Kind.MEAT, false),
+				new Dish("7", "D7", 818, Kind.MEAT, false));
+	}
+	
+	public static List<Transaction> getTransactions() {
+		Trader raoul = new Trader("Raoul", "Cambridge");
+		Trader mario = new Trader("Mario", "Milan");
+		Trader alan = new Trader("Alan", "Cambridge");
+		Trader brian = new Trader("Brian", "Cambridge");
+		List<Transaction> transactions = Arrays.asList(
+			new Transaction(brian, 2011, 300),
+			new Transaction(raoul, 2012, 1000), 
+			new Transaction(raoul, 2011, 400),
+			new Transaction(mario, 2012, 710), 
+			new Transaction(mario, 2012, 700), 
+			new Transaction(alan, 2012, 950)
+		);
+		return transactions;
+	}
+}
