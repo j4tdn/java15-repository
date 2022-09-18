a9e75a77f1b7d179a5d28b116fcbe5f85c8f8327
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-09-18 18:11:11 +0700

    lesson12-stream 18.09.2022

---
+++ lesson12-stream/src/view/Ex08Allocation.java
@@ -0,0 +1,37 @@
+package view;
+
+public class Ex08Allocation {
+	public static void main(String[] args) {
+		// 1. Get all stores which have stock previous day greater than 15
+		
+		// 2. Get all stores which have stock previous day greater than expected sales today
+		// and sort them store id descending
+		
+		// 3. Get stores have no reference store and return list of store ids
+		
+		// 4. Distinct stores by stock previous day
+		
+		// 5. Get first 2 stores with max expected sales 
+		
+		// 6. Get list of reference stores
+		
+		// 7. Is there any store has no expected sales
+		
+		/* 
+		 * 8. Calculate and return list of store's expected sales which has no expected sales
+		 * - store has not expected sales but
+		 *   + has reference store which maintained expected sales 
+		 *     --> use expected sales of reference store
+		 *   + has reference store which no maintained expected sales 
+		 *     --> use average of store's expected sales with it's own(reference store not count)
+		 *     
+		 *   + has reference store which no maintained expected sales 
+		 *     --> use average of store's expected sales with it's own(reference store not count)
+		 */
+		
+		// 9. Calculate sum of expected sales today
+		
+		// 10. Find store which has max stock previous day
+		// if many stores have same stock previous day, use the one with smallest store id
+	}
+}
