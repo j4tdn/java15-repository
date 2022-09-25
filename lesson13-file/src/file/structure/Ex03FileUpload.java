fb71d2195ab40244a2903c511328e63055f09fb8
ttngochuyen <ngochuyen26301@gmail.com>
2022-09-25 19:22:25 +0700

    lesson13-file 25.09.2022

---
+++ lesson13-file/src/file/structure/Ex03FileUpload.java
@@ -0,0 +1,33 @@
+package file.structure;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import utils.FileUtils;
+
+public class Ex03FileUpload {
+	private static String sourcePathName = String.join(File.separator, 
+					"storage", "template", "images","girl-with-candle-lantern-autumn.jpg");
+	
+	private static String targetPathName = String.join(File.separator, 
+			"storage", "template", "upload");
+	
+	public static void main(String[] args) {
+		File sourceFile = new File(sourcePathName);
+		//File targetFile = new File(targetPathName);
+		File targetFile = FileUtils.createNewDir(targetPathName);
+		
+		Path source = sourceFile.toPath();
+		Path target = targetFile.toPath();
+		
+		Path result = null;
+		try {
+			result = Files.copy(source, target.resolve(sourceFile.getName()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("result " + result.getFileName());
+	}
+}
---
+++ lesson13-file/src/utils/FileUtils.java
@@ -0,0 +1,98 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class FileUtils {
+	private static Random rd = new Random();
+	
+	private FileUtils() {
+		
+	}
+	
+	public static File createRandomFile(String dirPath, int nof, String ... extensions ) {
+		File dir = createNewDir(dirPath);
+		
+		for(int i = 0; i < nof; i++) {
+			String ext = extensions[rd.nextInt(extensions.length)];
+			String filename = "f" + System.currentTimeMillis() + rd.nextInt(100)
+							+ "e" + "." + ext;
+			File file = createNewFile(dir, filename);
+			System.out.println(
+				file.isFile() ? "File" + file.getName() + "isCreated ..."
+								: "cannot create file " + file.getName() 
+			);
+		}
+		
+		return dir;
+	}
+	
+	public static Path copy(String sourcePathName, String targetPathName) {
+		Path source = Paths.get(sourcePathName);
+		Path target = createNewDirAsPath(targetPathName);
+		
+		Path targetPath = null;
+		try {
+			targetPath = Files.copy(source, target.resolve(renameTo(source.getFileName())));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return targetPath;
+	}
+	
+	public static Path renameTo(Path source) {
+		String filename = source.toString();
+		String renameTo = "rnf" + System.currentTimeMillis() 
+							+ new Random().nextInt(100)
+							+ filename.substring(filename.lastIndexOf("."));
+		return Paths.get(renameTo);
+	}
+	
+	public static File createNewFile(String pathname) {
+		File file = new File(pathname);
+		File parent = createNewDir(file.getParent());
+		if(parent.isDirectory()) {
+			if(!file.exists()) {
+				try{
+					boolean flag = file.createNewFile();
+					if(flag) {
+						System.out.println("Folder " + file.getName() + " created successful");
+					}
+				}catch(IOException e) {
+					e.printStackTrace();
+				}
+			}else {
+				System.out.println("File " + file.getName() + " existed!!!");
+			}
+		}
+		return file;
+	}
+	
+	//parent; a/b/c
+	//child: x.txt
+	public static File createNewFile(File parent, String child) {
+		return createNewFile(parent.getAbsoluteFile() + File.separator + child);
+	}
+	
+	public static File createNewDir(String pathname) {
+		File dir = new File(pathname);
+		if(!dir.exists()) {
+			boolean flag = dir.mkdirs();
+			if(flag) {
+				System.out.println("Folder " + dir.getName() + " created successful");
+			}
+		}else {
+			System.out.println("File " + dir.getName() + " existed!!!");
+		}
+		return dir;
+	}
+	
+	public static Path createNewDirAsPath(String pathname) {
+		return createNewDir(pathname).toPath();
+	}
+}
