712b345e464e0bf181bbc961313a4530de915e15
dangnhan2510 <nhan200182@gmail.com>
2022-11-25 21:10:11 +0700

    internship 25-11-2022

---
+++ internship-d1-dangnhan/src/view/Ex01.java
@@ -0,0 +1,7 @@
+package view;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ internship-d1-dangnhan/src/view/Ex02.java
@@ -0,0 +1,29 @@
+package view;
+
+public class Ex02 {
+	public static void main(String[] args) {
+
+		int number = 19;
+		int result = number;
+		while (result != 1 && result != 4) {
+			result = isHappyNumber(result);
+		}
+		if (result == 1)
+			System.out.println(number + " is a happy number");
+		
+		else if (result == 4)
+			System.out.println(number + " is not a happy number");
+	}
+
+	public static int isHappyNumber(int number) {
+		int rem = 0, sum = 0;
+
+		// tính bình phương các chữ số
+		while (number > 0) {
+			rem = number % 10;
+			sum = sum + (rem * rem);
+			number = number / 10;
+		}
+		return sum;
+	}
+}
